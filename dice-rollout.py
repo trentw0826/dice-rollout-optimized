141b2133b9c259fca54dd5e07e5e9ce68c2a156b
trentw0826 <trentw0826@gmail.com>
2026-01-01 15:33:43 -0500

    Remove default parameter for cleaner future JIT compilation

--- dice-rollout.py
+++ dice-rollout.py
@@ -5,7 +5,7 @@ import time
 NUM_DICE = 6
 NUM_SIDES = 6
 TARGET_FACE_VALUE = 6 
-DEFAULT_TRIALS = 1000000
+NUM_TRIALS = 1000000
 
 @njit
 def rollout():
@@ -24,16 +24,15 @@ def rollout():
     return rolls
 
 @njit(parallel=True)
-def monte_carlo(trials=DEFAULT_TRIALS):
+def monte_carlo(trials):
     totals = np.zeros(trials, dtype=np.int32)
     for i in prange(trials):
         totals[i] = rollout()
     return totals.sum() / trials
 
-if __name__ == "__main__":
-    
+if __name__ == "__main__":    
     start_time = time.time()
-    average = monte_carlo()
+    average = monte_carlo(NUM_TRIALS)
     elapsed_time = time.time() - start_time
     
     print(f"Calculated average of {average} in {elapsed_time:.4f} seconds")
