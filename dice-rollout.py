16a8a4a15b7bb6f8ef325cf59c74e5414a9b6017
trentw0826 <trentw0826@gmail.com>
2026-01-01 15:11:10 -0500

    Initial rollout simulation

---
+++ dice-rollout.py
@@ -0,0 +1,34 @@
+from random import randint
+
+NUM_DICE = 6
+NUM_SIDES = 6
+TARGET_FACE_VALUE = 6 
+DEFAULT_TRIALS = 10000
+
+def rollout():
+    non_goal_remaining = NUM_DICE
+    rolls = 0
+
+    while non_goal_remaining > 0:
+        successes = 0
+        for _ in range(non_goal_remaining):
+            roll = randint(1, NUM_SIDES)
+            rolls += 1
+            if roll == TARGET_FACE_VALUE:
+                successes += 1
+        non_goal_remaining -= successes
+
+    return rolls
+
+
+def monte_carlo(trials=DEFAULT_TRIALS):
+    total_rolls = 0
+    for _ in range(trials):
+        total_rolls += rollout()
+    average_rolls = total_rolls / trials
+    return average_rolls
+
+if __name__ == "__main__":
+    average = monte_carlo()
+    print(f"Average number of rolls to get all sixes: {average}")
+    
